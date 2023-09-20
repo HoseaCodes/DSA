ed215655c20fe1c15529e4b256c748f50f6022f1
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 06:41:34 -0500

    completed validAnagrams challenge

--- traversy-js-challenges/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ traversy-js-challenges/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,32 @@
-function validAnagrams() {}
+function validAnagrams2(str1, str2) {
+    if (str1.length !== str2.length) return false;
+    return str1.split("").sort().join("") === str2.split("").sort().join("");
+}
+function validAnagrams(str1, str2) {
+    if (str1.length !== str2.length) return false;
+    const map1 = mapAnagrams(str1)
+    const map2 = mapAnagrams(str2)
+     for (var [key, val] of map1) {
+        let map2Val = map2.get(key);
+        // in cases of an undefined value, make sure the key
+        // actually exists on the object so there are no false positives
+        if (map2Val !== val || (map2Val === undefined && !map2.has(key))) {
+            return false;
+        }
+    }
+    return true
+}
+
+function mapAnagrams (str1) {
+    let map = new Map();
+    for (let index = 0; index < str1.length; index++) {
+        if (!map.has(str1[index])) {
+            map.set(str1[index], 1)
+        } else {
+            map.set(str1[index], map.get(str1[index]) + 1)
+        }        
+    }
+    return map
+}
 
 module.exports = validAnagrams;
