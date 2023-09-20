20260a5ef6115a0b2b6c9468177617e943d62329
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 07:15:46 -0500

    completed isValidIPv4 challenge

--- traversy-js-challenges/03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ traversy-js-challenges/03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,10 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+    let ip = str.split(".")
+    if (ip.length !== 4) return false
+    for (let i = 0; i < ip.length; i++) {
+        if (ip[i] > 255 || ip[i][0] <=0) return false
+    }
+    return true
+};
 
 module.exports = isValidIPv4;
