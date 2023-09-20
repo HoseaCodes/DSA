7910c6758740e23ab525e6a41b7a54b461b40f2c
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 07:08:44 -0500

    completed challenge generateHashtag

--- traversy-js-challenges/03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
+++ traversy-js-challenges/03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
@@ -1,5 +1,6 @@
 const generateHashtag = require('./hashtag-generator');
 
-const result = generateHashtag('hello world');
+// const result = generateHashtag('hello world');
+const result = generateHashtag(' Hello there thanks for trying my Kata');
 
 console.log(result);
--- traversy-js-challenges/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ traversy-js-challenges/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,22 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+    if (str.length > 140 || str.length <= 0) return false;
+    let hash = str.split(" ")
+    for (let i = 0; i < hash.length; i++) {
+        if (hash[i]) {   
+            hash[i] = capitalizeFirstLetter(hash[i])
+        }
+    }
+    hash.unshift("#")
+    return hash.join("")
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+// Write a function called `generateHashtag` that takes a string as input 
+// and returns a hashtag-generated string according to the rules below. 
+//If the generated hashtag string is longer than 140 characters or 
+//the input/result is an empty string, the function should return `false`.
+
 
 module.exports = generateHashtag;
