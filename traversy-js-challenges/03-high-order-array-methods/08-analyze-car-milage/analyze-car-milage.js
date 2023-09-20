56072289369ceeb49428ec83bc0ca6b48ec363be
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 10:17:17 -0500

    completed analyzeCarMileage challenge

--- traversy-js-challenges/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ traversy-js-challenges/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,27 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    if (cars.length === 0) return
+    let totalMileage = 0
+    let highestMileageCar = cars[0]
+    let lowestMileageCar = cars[0]
+    for (let i = 0; i < cars.length; i++) {
+        totalMileage += cars[i].mileage
+        if (cars[i].mileage > highestMileageCar.mileage) {
+            highestMileageCar = cars[i]
+        }
+        if (cars[i].mileage < lowestMileageCar.mileage) {
+            lowestMileageCar = cars[i]
+        }
+    }
+    console.log({averageMileage: (totalMileage / cars.length).toFixed(2),
+        highestMileageCar,
+        lowestMileageCar,
+        totalMileage})
+    return {
+        averageMileage: parseFloat((totalMileage / cars.length).toFixed(2)),
+        highestMileageCar: highestMileageCar,
+        lowestMileageCar: lowestMileageCar,
+        totalMileage: parseFloat(totalMileage)
+    }
+}
 
 module.exports = analyzeCarMileage;
