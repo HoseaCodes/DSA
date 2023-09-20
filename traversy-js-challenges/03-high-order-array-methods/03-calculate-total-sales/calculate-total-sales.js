d08f03398773a00fc616c776a112e892c9b13e96
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 06:17:42 -0500

    completed calculateTotalSalesWithTax challenge

--- traversy-js-challenges/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ traversy-js-challenges/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxes) {
+    let total = 0;
+    for (let index = 0; index < products.length; index++) {
+        const product = products[index];
+        const price = product.price * product.quantity
+        total += price;
+    }
+    const calculatedTaxes = total * (taxes / 100);
+    return (parseFloat(total + calculatedTaxes))
+}
 
 module.exports = calculateTotalSalesWithTax;
