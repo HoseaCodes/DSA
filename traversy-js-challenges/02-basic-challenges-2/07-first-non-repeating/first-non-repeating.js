bb5379f89d1411fced675166571e9cd72052a411
Dominique Hosea <dominique11h@yahoo.com>
2023-09-19 20:55:02 -0500

    completed findFirstNonRepeatingCharacter challenge

--- traversy-js-challenges/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ traversy-js-challenges/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,26 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  // Create a new map
+  const charCount = new Map();
+
+  // Loop through the string
+  for (const char of str) {
+    // If the map already has the current character, increment the count
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  // Loop through the string
+  for (const char of str) {
+    // If the count of the current character is 1, return the current character
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  // If no characters are repeated, return null
+  return null;
+}
+
+
+findFirstNonRepeatingCharacter('aabccdeff')
 
 module.exports = findFirstNonRepeatingCharacter;
