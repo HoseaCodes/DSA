7ab0651ca2c75d25429158c54d5c9663edc6204e
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 05:44:23 -0500

    completed validateEmail challenge

--- traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
+++ traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,19 @@
-function validateEmail() {}
+function validateEmail(email) {
+    if (email.indexOf('@') === -1) {
+        return false
+    } 
+
+    const [local, domain] = email.split('@')
+    if (local.length === 0 || domain.length < 3) {
+        return false;
+    }
+
+    const domainExt = domain.split('.');
+    if (domainExt.length < 2 || domainExt[1] < 2) {
+        return false
+    }
+
+    return true
+}
 
 module.exports = validateEmail;
