0fcbd12b155efdb6ecc84e385bb07ca26b0665ab
Dominique Hosea <dominique11h@yahoo.com>
2023-09-19 21:08:48 -0500

    completed diceGameSimulation challenge

--- traversy-js-challenges/02-basic-challenges-2/08-dice-game/dice-game.js
+++ traversy-js-challenges/02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,23 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+    let simulations = []
+    for (let i = 0; i < numSimulations; ++i) { 
+        let dice1 = Math.floor((Math.random() * 6) + 1)
+        let dice2 = Math.floor((Math.random() * 6) + 1)
+        let simulation = {
+            dice1: dice1,
+            dice2: dice2,
+            sum: dice1 + dice2,
+            result: dice1 + dice2 === 7 || dice1 + dice2 === 11 ? "win" :
+             dice1 + dice2 === 2 || dice1 + dice2 === 3 || dice1 + dice2 === 12 ? "lose" :
+             "roll again"
+        }
+        simulations.push(simulation)
+    }
+    console.log(simulations)
+
+    return simulations;
+}
+
+diceGameSimulation(5)
 
 module.exports = diceGameSimulation;
