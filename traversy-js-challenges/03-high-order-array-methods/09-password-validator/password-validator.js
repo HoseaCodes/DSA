622ad733a18b0e8cf445f707554d9c6c2e8c9bba
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 10:34:44 -0500

    completed validatePassword challenge

--- traversy-js-challenges/03-high-order-array-methods/09-password-validator/password-validator.js
+++ traversy-js-challenges/03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,25 @@
-function validatePassword() {}
+function validatePassword(password) {
+    if (password.length < 8) return false;
+    let uppercase = false;
+    let lowercase = false;
+    let digit = false;
+    for (let i = 0; i < password.length; i++) {
+        if (Number.isInteger(parseInt(password[i]))) {
+            digit = true
+            console.log(password[i], {digit})
+        } else {
+            if (password[i] === password[i].toUpperCase()) {
+                uppercase = true
+                console.log(password[i], {uppercase})
+            }
+            if (password[i] === password[i].toLowerCase()) {
+                console.log(password[i], {lowercase})
+                lowercase = true
+            }
+        }
+    }
+    console.log(uppercase, lowercase, digit)
+    return uppercase && lowercase && digit ? true : false;
+}
 
 module.exports = validatePassword;
