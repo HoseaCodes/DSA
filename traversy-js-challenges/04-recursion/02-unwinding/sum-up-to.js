0f0b3d9864e4bd6f40c62a9939a8974cec7c66e2
Dominique Hosea <dominique11h@yahoo.com>
2023-09-20 21:19:39 -0500

    completed sumUpTo challenge

--- traversy-js-challenges/04-recursion/02-unwinding/sum-up-to.js
+++ traversy-js-challenges/04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,13 @@
-function sumUpTo() {}
+function sumUpTo(num) {
+    if (num === 1) {
+        // console.log(`edge case - sumUpTo(${num}) return 1`)
+        return 1
+    }
+    // console.log(`sumUpTo(${num}) calls sumUpTo(${num - 1}) + ${num}`)
+    // const res = num + sumUpTo(num - 1);
+    // console.log(`sumUpTo(${num}) return ${res}`)
+    // return res
+    return num + sumUpTo(num - 1);
+}
 
 module.exports = sumUpTo;
