8962a07a4406bc926b6b824a2523337adcf8c450
Dominique Hosea <dominique11h@yahoo.com>
2023-09-21 05:58:10 -0500

    completed permutations challenge

--- traversy-js-challenges/04-recursion/10-permutations/permutations.js
+++ traversy-js-challenges/04-recursion/10-permutations/permutations.js
@@ -1,3 +1,22 @@
-function permutations() {}
+function permutations(str) {
+    let res = [];
+
+    if (str.length === 0) {
+        res.push('');
+        return res;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        const firstChar = str[i]
+        const restOfStr = str.slice(0, i) + str.slice(i + 1);
+        const subPermutations = permutations(restOfStr);
+
+        for (let j = 0; j < subPermutations.length; j++) {
+            res.push(firstChar + subPermutations[j])
+        }
+    }
+
+    return res;
+}
 
 module.exports = permutations;
