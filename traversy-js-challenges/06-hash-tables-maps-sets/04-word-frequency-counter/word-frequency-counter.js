9e7d9c91372f784e80a2d7aea93d2854ea1cf629
Dominique Hosea <dominique11h@yahoo.com>
2023-09-21 21:29:01 -0500

    completed wordFrequencyCounter challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,16 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+    const wordsArray = str.toLowerCase().split(/\W+/);
+    const map = new Map();
+
+    for (let i = 0; i < wordsArray.length; i++) {
+        if (wordsArray[i] === '') continue;
+        if (!map.has(wordsArray[i])) {
+            map.set(wordsArray[i], 1)
+        } else if (map.has(wordsArray[i])) {
+            map.set(wordsArray[i], map.get(wordsArray[i]) + 1)
+        }
+    }
+    return map;
+}
 
 module.exports = wordFrequencyCounter;
