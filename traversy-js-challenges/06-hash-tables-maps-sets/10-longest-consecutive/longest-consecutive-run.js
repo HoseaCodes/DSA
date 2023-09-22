9bb03c3d6e9ef45cc1e08e79201147e6d142a734
Dominique Hosea <dominique11h@yahoo.com>
2023-09-21 22:43:10 -0500

    completed longestConsecutiveSequence challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive-run.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive-run.js
@@ -1,5 +1,6 @@
 const longestConsecutiveSequence = require('./longest-consecutive');
 
-const result = longestConsecutiveSequence([100, 4, 200, 1, 3, 2]);
+// const result = longestConsecutiveSequence([100, 4, 200, 1, 3, 2]);
+const result = longestConsecutiveSequence([0, 3, 7, 2, 5, 8, 4, 6, 9, 1]);
 
 console.log(result);
--- traversy-js-challenges/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,24 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+    let sortedNums = nums.sort((a, b) => a - b)
+    let count = 0;
+    const set = new Set()
+    for (let i = 0; i < sortedNums.length - 1; i++) {
+        console.log(sortedNums[i], sortedNums[i + 1])
+        if (sortedNums[i] + 1 === (sortedNums[i + 1])) {
+            count++;
+            console.log(true)
+        } else {
+            set.add(count + 1)
+            count = 0
+        }
+    }
+
+    let max = 0
+    for (const num of set) {
+        if (num > max) max = num
+    }
+
+    return Math.max(max, count + 1)
+}
 
 module.exports = longestConsecutiveSequence;
