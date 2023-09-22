967439fcf31b1baa5e6a7781457ba64fb74c713d
Dominique Hosea <dominique11h@yahoo.com>
2023-09-21 22:58:57 -0500

    started creating custom HashTable

--- traversy-js-challenges/06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,59 @@
-class HashTable {}
+class HashTable {
+    constructor(limit = 14) {
+        this.storage = []
+        this.limit = limit
+    }
+
+      // Hash function
+  _hash(key, max) {
+    // Initialize the hash variable to 0
+    let hash = 0;
+    // Iterate through the key
+    for (let i = 0; i < key.length; i++) {
+      // Add the character code at each iteration to the hash variable
+      hash += key.charCodeAt(i);
+    }
+    // Return the hash modulo the max
+    return hash % max;
+  }
+
+   // Insert a key-value pair into the hash table
+  set(key, value) {
+    // Hash the key
+    const index = this._hash(key, this.limit);
+    // If the index is empty, insert the key-value pair
+    if (this.storage[index] === undefined) {
+      // create a bucket
+      this.storage[index] = [[key, value]];
+    } else {
+      //  If the index is not empty, iterate through the bucket (collision handling)
+      let inserted = false;
+
+      for (let i = 0; i < this.storage[index].length; i++) {
+        // If the key exists, update the value
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      // If the key does not exist, insert the key-value pair
+      if (inserted === false) {
+        this.storage[index].push([key, value]);
+      }
+    }
+  }
+
+
+    // Print all keys/values in the table
+   printTable() {
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage[i] !== undefined) {
+        console.log(`Bucket ${i}: ${JSON.stringify(this.storage[i])}`);
+      } else {
+        console.log(`Bucket ${i} Empty`);
+      }
+    }
+  }
+}
 
 module.exports = HashTable;
