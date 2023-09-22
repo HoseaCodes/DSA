6ec4b18fde19409ec4d81795a6067859e9004dd4
Dominique Hosea <dominique11h@yahoo.com>
2023-09-21 21:40:32 -0500

    completed phoneNumberDirectory challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,10 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+    const map = new Map();
+    for (const phoneNumber of arr) {
+        let phone = phoneNumber.split(":") 
+        map.set(phone[0], phone[1])  
+    }
+    return map
+}
 
 module.exports = phoneNumberDirectory;
