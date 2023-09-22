7ab07dc4bc0306b7c93f7a9c86758259159514ad
Dominique Hosea <dominique11h@yahoo.com>
2023-09-21 22:26:33 -0500

    completed twoSum challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,13 @@
-function twoSum() {}
+function twoSum(arr, target) {
+  const numSet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    if (numSet.has(complement)) {
+      return [arr.indexOf(complement), i];
+    }
+    numSet.add(arr[i]);
+  }
+    return []
+}
 
 module.exports = twoSum;
